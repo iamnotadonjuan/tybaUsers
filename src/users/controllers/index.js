5f761b5806be29f7c49364387f6b1442fe12c053
jcamargo <juancamargo93@outlook.com>
2021-05-16 08:56:40 -0500

    init project structure

---
+++ src/users/controllers/index.js
@@ -0,0 +1,24 @@
+const userService = require('../services')
+
+exports.signin = async (req, res, next) => {
+  try {
+    await userService();
+  } catch (error) {
+    
+  }
+}
+
+exports.login = async (req, res, next) => {
+  try {
+  } catch (error) {
+    
+  }
+}
+
+exports.logout = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
